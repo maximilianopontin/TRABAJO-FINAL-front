cbd6c4d24f4aac44f09afc0949e5658175f754ae
maximilianopontin <maxipontin92@gmail.com>
2024-04-24 21:07:18 -0300

    creacion proyecto react

--- SoundGood front/src/App.jsx
+++ SoundGood front/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PagInicio  from './Componentes/Pagina de Inicio/PagInicio';
+
 
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div> 
+     <PagInicio/>
      
+     </div>
     </>
   )
 }
---
+++ SoundGood front/src/Componentes/Pagina de Inicio/PagInicio.jsx
@@ -0,0 +1,16 @@
+//en esta seccion se muestra la pagina de inicio para registro e inicio de seccion
+import logo from '../../logo/logo.png';
+
+function PagInicio(){
+    
+
+return (
+<div>
+<img src= {logo} alt="logo" />
+ 
+</div>
+);
+}
+
+
+export default PagInicio;
