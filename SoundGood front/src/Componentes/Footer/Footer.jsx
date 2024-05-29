f6740132ef2c528daab42b27f654282cae0f4c1b
maximilianopontin <maxipontin92@gmail.com>
2024-05-29 20:46:10 -0300

    cambios en footer

--- SoundGood front/src/Componentes/Footer/Footer.jsx
+++ SoundGood front/src/Componentes/Footer/Footer.jsx
@@ -9,32 +9,28 @@ function Footer() {
             <div class="container">
                 <div class="footer-row">
                     <div class="footer-links">
-                        <h4 class="footer-title">Ayudas</h4>
+                        <h4 class="footer-title">Compañia</h4>
                         <ul>
-                            <li><a href="#">Preguntas</a></li>
-                            <li><a href="#">Compras</a></li>
-                            <li><a href="#">Servicios de atención al cliente</a></li>
-                            <li><a href="#">Estatus de orden</a></li>
-                            <li><a href="#">Pago</a></li>
+                            <li><a href="#">Acerca de</a></li>
+                            <li><a href="#">Empleo</a></li>
+                            
                         </ul>
                     </div>
                     <div class="footer-links">
-                        <h4 class="footer-title">Tienda</h4>
+                        <h4 class="footer-title">Enlaces Utiles</h4>
                         <ul>
-                            <li><a href="#">Excursiones</a></li>
-                            <li><a href="#">Alquiler de coches</a></li>
-                            <li><a href="#">Reserva de vuelos</a></li>
-                            <li><a href="#">Reserva de alojamiento</a></li>
-                            <li><a href="#">Seguros de viajes</a></li>
-                            <li><a href="#">Asistencia de visas y documentos</a></li>
+                            <li><a href="#">Plan Premium</a></li>
+                            <li><a href="#">Version Gratuita</a></li>
+                            <li><a href="#">Ayudas</a></li>
+                           
                         </ul>
                     </div>
                     <div class="footer-links">
                         <h4 class="footer-title">Síguenos</h4>
                         <div class="social-link">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.facebook.com/?locale=es_LA"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                            <a href="https://twitter.com/i/flow/signup"><i class="fab fa-twitter"></i></a>
                         </div>
                     </div>
                 </div>
