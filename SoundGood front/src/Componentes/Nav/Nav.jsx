257f948115507f37c886dfd3d4c00c6d5aed1ea5
LudmilaMiguens <miguensludmila2@gamil.com>
2024-06-05 21:58:36 -0300

    probando el nav con el json top 50

--- SoundGood front/src/Componentes/Nav/Nav.jsx
+++ SoundGood front/src/Componentes/Nav/Nav.jsx
@@ -1,33 +1,38 @@
-import { Logo } from "../../logo/logo"
-import './Nav.css'
-import { useState } from "react"
+import { Logo } from "../../logo/logo";
+import './Nav.css';
+import './modal.css';
+import { useState } from "react";
+
 export const Nav = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
-    // constante que recorre el json y filtra los resultados
+    const [isModalOpen, setModalOpen] = useState(false);
+
     const handleSearch = () => {
         if (searchTerm.trim() === '') {
-            return; // no hacer nada si el campo de búsqueda está vacío
+            return;
         }
-        fetch('canciones.json')
+        fetch('CancionesTop50.json')
             .then(response => response.json())
             .then(data => {
                 const filteredResults = data.filter(song =>
-                    song.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+                    song.title.toLowerCase().includes(searchTerm.toLowerCase())
                 );
                 setSearchResults(filteredResults);
+                setModalOpen(true);
                 setSearchTerm('');
             })
             .catch(error => console.error('Error al obtener datos:', error));
     };
-    // constante para hacer funcionar la tecla enter para la busqueda
+
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             handleSearch();
         }
     };
+
     return (
-        <nav >
+        <nav>
             <div className="navbar">
                 <div className="nav-logo">
                     <Logo />
@@ -37,7 +42,7 @@ export const Nav = () => {
                         placeholder="Buscar..."
                         value={searchTerm}
                         onChange={e => setSearchTerm(e.target.value)}
-                        onKeyDown={handleKeyDown} /> {/* Buasca la cancion precionando la tecla enter */}
+                        onKeyDown={handleKeyDown} />
                     <button type="button" onClick={handleSearch}>Buscar</button>
                 </div>
                 <div className="nav-links">
@@ -45,21 +50,23 @@ export const Nav = () => {
                     <a href="">Cuenta</a>
                 </div>
             </div>
-            {/*Se muestran los resultados de la busqueda*/}
-            <div className="cancionesEncontradas">
-                {searchResults.length > 0 && (
-                    <div>
-                        <h2>Canciones encontradas: </h2>
-                        <ul>
-                            {searchResults.map((song, index) => (
-                                <p key={index}>
-                                    <a href={song.url}>{song.nombre}</a>
-                                </p>
-                            ))}
-                        </ul>
+            {isModalOpen && (
+                <div className="modal-overlay">
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={() => setModalOpen(false)}>×</span>
+                            <h2>Canciones encontradas:</h2>
+                            <ul>
+                                {searchResults.map((song, index) => (
+                                    <li key={index}>
+                                        <a href={song.url}>{song.nombre}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                )}
-            </div>
+                </div>
+            )}
         </nav>
-    )
-}
+    );
+};
