910fe36d26de8b3f65f5d208cf81e88da6ea48f0
LudmilaMiguens <miguensludmila2@gamil.com>
2024-06-12 19:39:48 -0300

    mergeo de pagina Registro

--- SoundGood front/src/App.jsx
+++ SoundGood front/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import PagInicio from './Componentes/Pagina de Inicio/PagInicio';
-//import  PagRegistro  from './Componentes/Registro/Registro';
+import { PagRegistro } from './Componentes/Registro/Registro';
 import {PagInicioSesion} from './Componentes/Iniciar sesion/InicioSesion';
 import Footer from "./Componentes/Footer/Footer";
 import { Nav } from './Componentes/Nav/Nav';
@@ -20,20 +20,23 @@ function App() {
         // funcion que actualiza el estado count para redireccionar a la página de registro
         // setCount(2) para mostrar la página de registro.
     }
+
     const redirectToHome = () =>{
         setCount(3);
         // funcion que actualiza el estado count para redireccionar a la página de inico
         // setCount(3) para mostrar la pagina de inico.
     }
 
+
     return (
 
            <>
            <Nav/>
             <div>
+ 
                 {count === 0 && <PagInicio redirectToInicioSesion={redirectToInicioSesion} redirectToRegistro={redirectToRegistro} />} {/* Rediccionamineto de la pagina Inicio a Registro y Inico Sesion*/}
                 {count === 1 && <PagInicioSesion redirectToHome={redirectToHome}/>} {/* Rediccionamineto de la pagina Inicio sesio a Home*/}
-                {count === 2 && <PagRegistro />} {/* Aquí importamos y usamos el componente PagRegistro */}
+                {count === 2 && <PagRegistro redirectToInicioSesion={redirectToInicioSesion}/>} {/* Aquí importamos y usamos el componente PagRegistro */}
             </div>
 
             <div>
---
+++ SoundGood front/src/Componentes/Registro/Registro.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import logo from '../../logo/logo.png'
+import './Registro.css'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+//import registro from '../../../public/registro.json'
+
+export function PagRegistro({redirectToInicioSesion}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [fechaNacimiento, setFechaNacimiento] = useState(null);
+
+    //validamos que la contraseña y email cumpla con las condiciones dadas.
+    const handlerFormSudmit = (e) => {
+        e.preventDefault();
+
+        // validamos el email
+        if (!validateEmail(email)) {
+            setErrorMessage('El email debe ser valido');
+            return;
+        }
+        //validamos la contraseña
+        if (!validatePassword(password)) {
+            setErrorMessage('La contraseña debe tener al menos una mayuscula, un numero y simbolo.');
+            return;
+        }
+        // Validaciones de nombre
+        if (nombre.trim() === '') { // el meto TRIM() limpia los espacios que pude llegar a poner el usuario
+            setErrorMessage('Por favor ingresa tu nombre.');
+            return;
+        }
+        // Validación de la fecha de nacimiento
+        if (!fechaNacimiento) {
+            setErrorMessage('Por favor, selecciona tu fecha de nacimiento.');
+            return;
+        }
+        //constante para poder cargar el json ??????????????????????
+        const userData = {
+            email,
+            password,
+            nombre,
+            fechaNacimiento: fechaNacimiento ? fechaNacimiento.toISOString() : null
+        };
+        // const updatedClientes = [...registro.clientes, userData];
+        // const updatedRegistro = { clientes: updatedClientes };
+        // localStorage.setItem('registro', JSON.stringify(updatedRegistro));
+
+        clearFrom(); //si el form se envia correctamente, este se limpia
+    }
+    //Validacion para el email
+    const validateEmail = (email) => {
+        const conditionEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return conditionEmail.test(email);
+    }
+    //Validacion para las condiciones de la contraseña
+    const validatePassword = (password) => {
+        const conditionPassaword = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+        return conditionPassaword.test(password);
+    }
+    //Validamos sin el email cumple con las condicones
+    const hadleEmailChange = (e) => {
+        const newEmail = e.target.value;
+        setEmail(newEmail);
+        setErrorMessage('');
+    }
+
+    //validamos si la contraseña esta bien o mal
+    const handlePasswordChange = (e) => {
+        const newPassword = e.target.value
+        setPassword(newPassword);
+        if (validatePassword(newPassword)) {
+            setErrorMessage('');
+        } else {
+            setErrorMessage('La contraseña es incorrecta');
+        }
+    }
+
+    //Liampiar el form
+    const clearFrom = () => {
+        setEmail('');
+        setPassword('');
+        setErrorMessage('');
+        setNombre('');
+        setFechaNacimiento(null);
+
+    }
+
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <form className="form" onSubmit={handlerFormSudmit}>
+
+                <input type="text" name="email" placeholder="Email" value={email} onChange={hadleEmailChange} required />
+
+                <input type="password" name="password" placeholder="Contraseña" value={password} onChange={handlePasswordChange} required />
+
+                <div className="parrafos">
+                    <p>Para garantizar la seguridad de tu cuenta, tu contraseña debe cumplir con los siguientes requisitos:</p>
+                    <p>Debe contener al menos:</p>
+                    <p> Una letra mayúscula, un carácter especial, un número y varias letras minúsculas </p>
+                    <p>¡Gracias por tu cooperación en mantener tu cuenta segura!</p>
+                </div>
+
+                <input type="text" name="nombre" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+
+                <DatePicker /*Biblioteca para el calendario de la fecha \\npm install react-datepicker\\ */
+                    selected={fechaNacimiento}
+                    onChange={(date) => setFechaNacimiento(date)}
+                    placeholderText="Fecha de nacimiento"
+                    dateFormat="dd/MM/yyyy"
+                    className="date-picker"
+                />
+                <div className="parrafos">
+                    <p>Términos y Condiciones de SoundGood</p>
+                    <p>Al registrarte en SoundGood, aceptas nuestros términos y condiciones. Por favor, asegúrate de revisarlos periódicamente, ya que pueden cambiar. Gracias por unirte a nuestra comunidad musical.</p>
+                </div>
+
+
+
+                <button onClick={redirectToInicioSesion}>Registrarse</button>
+
+                <div className="error-message">
+                    {errorMessage ? errorMessage : ""}
+                </div>
+
+            </form>
+        </div>
+    )
+} 
