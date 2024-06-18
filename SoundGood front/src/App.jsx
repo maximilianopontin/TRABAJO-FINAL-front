f3dd9048cefe53f5bcc2162dba6a199659b57d30
maximilianopontin <maxipontin92@gmail.com>
2024-06-18 10:02:29 -0300

    creacion de link footer

--- SoundGood front/src/App.jsx
+++ SoundGood front/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   return (
     <>
       <div>
-       <Nav/>
-      <PagInicioSesion />
+      {/*<Nav/>*/} 
+      {/*<PagInicioSesion />*/}
       </div>
       <div>
         <Footer/>
