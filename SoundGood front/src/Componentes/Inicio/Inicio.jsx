1f77b0adc2fa55a94098fdec86eb58b917da5c2c
maximilianopontin <maxipontin92@gmail.com>
2024-06-19 20:13:22 -0300

    agregado de nav a inicio

--- SoundGood front/src/Componentes/Inicio/Inicio.jsx
+++ SoundGood front/src/Componentes/Inicio/Inicio.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Reproductor from '../Reproductor musica/Reproductor';
 import Footer from "./Componentes/Footer/Footer";
-
+import { Nav } from "../Nav/Nav";
 
 export function Inicio() {
     return (
         <>
-
+<div>
+    <Nav/>
+</div>
         <div>
                 <Reproductor />
             </div>
