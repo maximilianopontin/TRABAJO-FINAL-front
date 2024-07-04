38a4540ef9dd2e39ee7a2788054167e68664bb29
LudmilaMiguens <miguensludmila@gmail.com>
2024-07-04 19:18:31 -0300

    creacion de la rama Biblioteca

---
+++ SoundGood front/src/Componentes/Biblioteca/Biblioteca.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Nav} from '../Nav/Nav'
+
+export function Biblioteca () {
+    return (
+        <>
+        <div>
+            <Nav/> 
+        </div>
+        
+        </>
+
+    );
+}
