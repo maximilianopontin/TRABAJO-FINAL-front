e8185f52a4a6801413474f69e83f6be3381c4661
LudmilaMiguens <miguensludmila@gmail.com>
2024-07-10 22:21:29 -0300

    cuenta, editar-perfil y premium en proceso

--- SoundGood front/src/Componentes/Cuenta/Premium.jsx
+++ SoundGood front/src/Componentes/Cuenta/Premium.jsx
@@ -1,6 +1,28 @@
+import Footer from "../Footer/Footer"
+import { Nav } from "../Nav/Nav"
+
 function Premium() {
-    return(
-        <p>holaaaaaaaaa</p>
+    return (
+        <>
+            <div>
+                <Nav />
+            </div>
+            <div>
+                <form className="form" >
+                    <input type="text" name="N° de tarjeta" placeholder="N° de tarjeta" required />
+                    <input type="date" name="fecha" placeholder="Fecha" required />
+                    <input type="text" name="codigo" placeholder="codigo" required />
+                    <button type="submit" className="btn">Pagar</button>
+                </form>
+            </div>
+
+
+
+
+            <div>
+                <Footer />
+            </div>
+        </>
     )
 }
 export default Premium
